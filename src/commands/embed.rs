8a807df23eeebe1a8bfd7130411f2050508e4425
Ashlynn <49912456+Dastille@users.noreply.github.com>
2025-07-24 21:22:29 -0400

    Delete src/commands directory
    New version

--- src/commands/embed.rs
+++
@@ -1,2 +0,0 @@
-use anyhow::Result;
-pub fn run(input: &str, archive: &str, output: &str) -> Result<()> { println!("Embedding {} into {} as {}", archive, input, output); Ok(()) }
--- src/commands/prune.rs
+++
@@ -1,2 +0,0 @@
-use anyhow::Result;
-pub fn run(archive: &str, keep_latest: usize) -> Result<()> { println!("Pruning {} to retain {} versions", archive, keep_latest); Ok(()) }
